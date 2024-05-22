fa0a06a99f5d2f1147336a0e6227a08caacccee4
artemykust <kus.tyoma@gmail.com>
2024-05-22 12:45:03 +0400

    a

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
-import mkcert from 'vite-plugin-mkcert'
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     watch: {
-      usePolling: true
-    }
+      usePolling: true,
+    },
   },
   resolve: {
     alias: {
